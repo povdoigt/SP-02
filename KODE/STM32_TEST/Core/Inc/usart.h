4e6a8bb48a5958a55e4f41327fe3a7cf5178e97e
LeProfDeGitHub <alexis.paillard@outlook.fr>
2025-11-21 17:12:32 +0100

    WORK ON STM32 KODE

--- KODE/STM32_TEST/Core/Inc/usart.h
+++ KODE/STM32_TEST/Core/Inc/usart.h
@@ -32,13 +32,21 @@ extern "C" {
 
 /* USER CODE END Includes */
 
+extern UART_HandleTypeDef huart1;
+
 extern UART_HandleTypeDef huart2;
+
+extern UART_HandleTypeDef huart3;
 
 /* USER CODE BEGIN Private defines */
 
+extern uint8_t rx_data[257];
+
 /* USER CODE END Private defines */
 
+void MX_USART1_UART_Init(void);
 void MX_USART2_UART_Init(void);
+void MX_USART3_UART_Init(void);
 
 /* USER CODE BEGIN Prototypes */
 
